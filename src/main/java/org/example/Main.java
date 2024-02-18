c73fee6eb1b875221adb050ee9770c2ab06100ec
fernandokkang <spineattack@gmail.com>
2024-02-19 01:02:56 +0900

    feat: Algorithm to find parentheses pairs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,43 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Stack;
+
+public class Main {
+
+    private static Stack<Character> stack;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String [] input = {"((())())", "(()()))(", "((())"};
+
+        for(int i=0; i< input.length; i++) {
+
+            stack = new Stack<>();
+            System.out.println(solution(input[i]));
+        }
+    }
+
+    private static boolean solution(String s) {
+
+        if(s.length() % 2 != 0) {
+            return false;
+        }
+
+        for(int i=0; i<s.length(); i++) {
+
+            if(stack.empty()) {
+                stack.push(s.charAt(i));
+            } else{
+                if(s.charAt(i) == ')') {
+                    if(stack.peek() == '(') {
+                        stack.pop();
+                    }
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+
+        return stack.empty();
     }
 }
